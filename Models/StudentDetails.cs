547dad106796938715913b913f5254d5e1aba9ef
frhn16 <faroomirz@hotmail.com>
2024-09-02 16:11:24 +0100

    Add project files.

---
+++ Models/StudentDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models
+{
+    public class StudentDetails
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public required string Name { get; set; }
+        [Required]
+        [Range(5, 18)]
+        public int Age { get; set; }
+        [Required]
+        [Range(1, 13)]
+        public int Year { get; set; }
+        [Required]
+        [StringLength(15)]
+        public required string MobileNumber { get; set; }
+        [Required]
+        public DateOnly DOB { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+using StudentManagementSystem.Components;
+using StudentManagementSystem.Models; // Added here so that the functions in these folders can be used across the whole project
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
