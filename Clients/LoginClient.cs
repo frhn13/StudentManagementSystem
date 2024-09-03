65a3b9f8fcfa12012c85748c13ccc44c20aa7962
frhn16 <faroomirz@hotmail.com>
2024-09-04 00:26:52 +0100

    Created sign up form and added all validation for making a student account

---
+++ Clients/LoginClient.cs
@@ -0,0 +1,6 @@
+﻿namespace StudentManagementSystem.Clients
+{
+    public class LoginClient
+    {
+    }
+}
---
+++ Models/SignUpDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models
+{
+    public class SignUpDetails
+    {
+        [Required]
+        [StringLength(50)]
+        public required string Name { get; set; }
+        [Required]
+        [StringLength(50)]
+        public required string Username { get; set; }
+        [Required]
+        [MaxLength(15, ErrorMessage = "Password can't be more than 15 characters")]
+        [MinLength(6, ErrorMessage = "Password can't be less than 6 characters")]
+        public required string Password { get; set; }
+        [Required(ErrorMessage = "The Confirm Password field is required")]
+        [Compare("Password", ErrorMessage = "Passwords must be the same")]
+        public required string ConfirmPassword { get; set; }
+        public int UserId { get; set; }
+        public string? Role {  get; set; }
+    }
+}
