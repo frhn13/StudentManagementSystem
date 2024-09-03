bd4a9c07c9c0826518459348f70fa922411109ed
frhn16 <faroomirz@hotmail.com>
2024-09-03 20:59:05 +0100

    Can now route to edit pages and can delete students with proper routing

--- Clients/DataManagement.cs
+++ Clients/DataManagement.cs
@@ -20,22 +20,44 @@ namespace StudentManagementSystem.Clients
                 throw new ApplicationException("Oopsies!");
             }
         }
-        public static void DeleteStudent(StudentDetails studentDetails)
-        {
 
+        public static void DeleteStudent(int studentId)
+        {
+            string removedStudent = "";
+            try
+            {
+                string[] students = System.IO.File.ReadAllLines(@"students.csv");
+                foreach (string student in students)
+                {
+                    string[] studentData = student.Split(',');
+                    if (int.Parse(studentData[0]) == studentId)
+                    {
+                        removedStudent = student;
+                        break;
+                    }
+                }
+                students = students.Where(x => x != removedStudent).ToArray();
+                using (StreamWriter file = new StreamWriter(@"students.csv", false))
+                {
+                    for (int i=0; i < students.Length; i++) 
+                    {
+                        string[] studentData = students[i].Split(',');
+                        file.WriteLine($"{i+1},{studentData[1]},{studentData[2]},{studentData[3]},{studentData[4]},{studentData[5]}");
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new ApplicationException("Oopsies!");
+            }
         }
+
         public static void EditStudent(StudentDetails studentDetails)
         {
             try
             {
                 string[] students = System.IO.File.ReadAllLines(@"students.csv");
                 string[] editedStudent = students[studentDetails.Id].Split(",");
-                editedStudent[0] = studentDetails.Id.ToString();
-                editedStudent[1] = studentDetails.Name;
-                editedStudent[2] = studentDetails.Age.ToString();
-                editedStudent[3] = studentDetails.Year.ToString();
-                editedStudent[4] = studentDetails.MobileNumber;
-                editedStudent[5] = studentDetails.DOB.ToString();
                 students[studentDetails.Id-1] = $"{studentDetails.Id},{studentDetails.Name},{studentDetails.Age},{studentDetails.Year},{studentDetails.MobileNumber},{studentDetails.DOB}";
                 using (StreamWriter file = new StreamWriter(@"students.csv", false))
                 {
@@ -51,6 +73,7 @@ namespace StudentManagementSystem.Clients
                 throw new ApplicationException("Oopsies!");
             }
         }
+
         public static StudentDetails GetStudent(int Id)
         {
             try 
@@ -80,6 +103,7 @@ namespace StudentManagementSystem.Clients
             }
             return null;
         }
+
         public static List<StudentDetails> ViewAllStudents() 
         {
             List<StudentDetails> studentDetailsList = [];
