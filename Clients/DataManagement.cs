a1011abeb3cd1e3b264520ca850c6e1139b56fe9
frhn16 <faroomirz@hotmail.com>
2024-09-03 17:37:02 +0100

    Can now edit student details with updates saved to the CSV file

--- Clients/DataManagement.cs
+++ Clients/DataManagement.cs
@@ -24,9 +24,61 @@ namespace StudentManagementSystem.Clients
         {
 
         }
-        public static void editStudent(StudentDetails studentDetails)
+        public static void EditStudent(StudentDetails studentDetails)
         {
-
+            try
+            {
+                string[] students = System.IO.File.ReadAllLines(@"students.csv");
+                string[] editedStudent = students[studentDetails.Id].Split(",");
+                editedStudent[0] = studentDetails.Id.ToString();
+                editedStudent[1] = studentDetails.Name;
+                editedStudent[2] = studentDetails.Age.ToString();
+                editedStudent[3] = studentDetails.Year.ToString();
+                editedStudent[4] = studentDetails.MobileNumber;
+                editedStudent[5] = studentDetails.DOB.ToString();
+                students[studentDetails.Id-1] = $"{studentDetails.Id},{studentDetails.Name},{studentDetails.Age},{studentDetails.Year},{studentDetails.MobileNumber},{studentDetails.DOB}";
+                using (StreamWriter file = new StreamWriter(@"students.csv", false))
+                {
+                    foreach (string student in students)
+                    {
+                        string[] studentData = student.Split(',');
+                        file.WriteLine($"{studentData[0]},{studentData[1]},{studentData[2]},{studentData[3]},{studentData[4]},{studentData[5]}");
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new ApplicationException("Oopsies!");
+            }
+        }
+        public static StudentDetails GetStudent(int Id)
+        {
+            try 
+            { 
+                string[] students = System.IO.File.ReadAllLines(@"students.csv");
+                foreach (string student in students)
+                {
+                    string[] studentData = student.Split(',');
+                    if (int.Parse(studentData[0]) == Id)
+                    {
+                        StudentDetails details = new StudentDetails
+                        {
+                            Id = int.Parse(studentData[0]),
+                            Name = studentData[1],
+                            Age = int.Parse(studentData[2]),
+                            Year = int.Parse(studentData[3]),
+                            MobileNumber = studentData[4],
+                            DOB = DateOnly.Parse(studentData[5])
+                        };
+                        return details;
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new ApplicationException("Oopsies!");
+            }
+            return null;
         }
         public static List<StudentDetails> ViewAllStudents() 
         {
@@ -49,7 +101,7 @@ namespace StudentManagementSystem.Clients
                     studentDetailsList.Add(details);
                 }
             }
-            catch
+            catch (FileNotFoundException ex)
             {
                 throw new ApplicationException("Oopsies!");
             }
